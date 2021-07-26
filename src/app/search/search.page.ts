d5cd94c53a767422bda17db5c199ce8ce0454ad3
carinasylvester <carina.sylvester@outlook.com>
2021-07-26 17:41:54 -0400

    Imported modules

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,12 @@
+// Imported life cycle components
 import { Component, OnInit } from '@angular/core';
+// Imported data service
+import { DataService } from '../data.service';
+// Imported Storage
+import { Storage } from '@ionic/storage';
+// Imported LoadingController, NavController, NavParams 
+import { LoadingController, NavController, NavParams} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-search',
